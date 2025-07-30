545914398518e6dd7774c20bec99ad3b02816869
sunamo.cz <radek.jancik@sunamo.cz>
2025-07-30 23:29:30 +0200

    Auto-commit before force push

---
+++ RunnerUriWebServices/Program.cs
@@ -0,0 +1,26 @@
+﻿using SunamoUriWebServices;
+using SunamoUriWebServices.Ads;
+
+namespace RunnerUriWebServices;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        var p = AdsPhaRegion.Pha();
+        string whatToSearch = null;
+
+        //for (int i = 55; i < 61; i++)
+        //{
+        //    whatToSearch = "televize " + i + "\"";
+        //    //UriWebServices.SearchInAll(p.All, whatToSearch);
+        //    UriWebServices.OpenUri(UriWebServices.FromChromeReplacement(p.bazosCz, whatToSearch));
+        //}
+
+        whatToSearch = "TV stolek";
+        UriWebServices.SearchInAll(p.All, whatToSearch);
+        //UriWebServices.OpenUri(UriWebServices.FromChromeReplacement(p.hyperinzerceCz, whatToSearch));
+    }
+}
