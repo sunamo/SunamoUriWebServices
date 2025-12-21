570e04d080c6081a1d1f9a893652cd0dec3ab16d
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:44:22 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoUriWebServices/UriWebServicesClassesWeb1.cs
+++ SunamoUriWebServices/UriWebServicesClassesWeb1.cs
@@ -1,6 +1,5 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 namespace SunamoUriWebServices;
+
 using SunamoUriWebServices.Values;
 
 public partial class UriWebServices
